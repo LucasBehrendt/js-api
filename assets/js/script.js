d39be68dcb821a39080ae431d876c456aa9baeb9
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-04-13 10:33:23 +0000

    Add final code & tidy up

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,43 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
-document.getElementById("submit").addEventListener("click", postForm);
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+function processOptions(form) {
+    let options = form.getAll("options");
+    form.delete("options");
+    form.append("options", options);
+    
+    // let optArray = [];
+
+    // for (let entry of form.entries()) {
+    //     if (entry[0] === "options") {
+    //         optArray.push(entry[1]);
+    //     }
+    // }
+    // form.delete("options");
+    // form.append("options", optArray);
+    return form;
+}
+
+async function postForm(e) {
+    const form = processOptions(new FormData(document.getElementById("checksform")));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+    const data = await response.json();
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        displayException(data);
+        throw new Error(data.error);
+    }
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -12,32 +48,19 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
 
-function displayStatus(d) {
+function displayException(d) {
+    results = `The API returned status code ${d.status_code}<br>`;
+    results += `Error number: <b>${d.error_no}</b><br>`;
+    results += `Error text: <b>${d.error}</b>`;
+
     resultsModal.show();
-    document.getElementById("resultsModalTitle").innerText = "API Key Status";
-    document.getElementById("results-content").innerHTML = "<div>Your key is valid until:</div>" + d.expiry;
-}
-
-async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
-
-    const response = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-            "Authorization": API_KEY,
-        },
-        body: form
-    });
-    const data = await response.json();
-    if (response.ok) {
-        displayErrors(data);
-    } else {
-        throw new Error(data.error);
-    }
+    document.getElementById("resultsModalTitle").innerText = "An Exception Occured";
+    document.getElementById("results-content").innerHTML = results;
 }
 
 function displayErrors(d) {
@@ -55,4 +78,10 @@ function displayErrors(d) {
     resultsModal.show();
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
+}
+
+function displayStatus(d) {
+    resultsModal.show();
+    document.getElementById("resultsModalTitle").innerText = "API Key Status";
+    document.getElementById("results-content").innerHTML = "<div>Your key is valid until:</div>" + d.expiry;
 }
